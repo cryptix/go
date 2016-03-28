09488a167439b3df922c3f111b3ca9ff5bc1a2b2
Henry <cryptix@riseup.net>
2016-03-28 14:22:05 +0200

    proc: simpler StdioConn and optional stderr argument to StartStdioProcess

--- proc/conn.go
+++ proc/conn.go
@@ -9,71 +9,60 @@ import (
 )
 
 type StdioConn struct {
-	Out io.ReadCloser
-	In  io.WriteCloser
+	io.ReadCloser
+	io.WriteCloser
 }
 
-func (s StdioConn) Close() (err error) {
-
-	if err = s.In.Close(); err != nil {
+// Close calls Close on both closers
+func (s StdioConn) Close() error {
+	if err := s.ReadCloser.Close(); err != nil {
 		return err
 	}
-
-	if err = s.Out.Close(); err != nil {
+	if err := s.WriteCloser.Close(); err != nil {
 		return err
 	}
-
 	return nil
 }
 
-func (s *StdioConn) Read(p []byte) (n int, err error) {
-	// log.Println("called read")
-	n, err = s.Out.Read(p)
-	// log.Printf("read[%d] bytes '%s'", n, string(p))
-	return
-}
-
-func (s *StdioConn) Write(p []byte) (n int, err error) {
-	n, err = s.In.Write(p)
-	// log.Printf("wrote[%d] '%s'", n, string(p))
-	return
-}
-
-func StartStdioProcess(path string) (*StdioConn, error) {
+func StartStdioProcess(path string, stderr io.Writer, args ...string) (*StdioConn, error) {
 	var (
 		err  error
-		cmd  = exec.Command(path)
+		cmd  = exec.Command(path, args...)
 		conn StdioConn
 	)
 
-	conn.Out, err = cmd.StdoutPipe()
+	conn.ReadCloser, err = cmd.StdoutPipe()
 	if err != nil {
 		return nil, err
 	}
 
-	conn.In, err = cmd.StdinPipe()
+	conn.WriteCloser, err = cmd.StdinPipe()
 	if err != nil {
 		return nil, err
 	}
 
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return nil, err
+	if stderr == nil {
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			return nil, err
+		}
+
+		go func() {
+			s := bufio.NewScanner(stderr)
+			for s.Scan() {
+				fmt.Fprintln(os.Stderr, "stdioproc err:", s.Text())
+			}
+			if err := s.Err(); err != nil {
+				fmt.Fprintln(os.Stderr, "stdioproc failed:", err)
+			}
+		}()
+	} else {
+		cmd.Stderr = stderr
 	}
 
 	if err = cmd.Start(); err != nil {
 		return nil, err
 	}
-
-	go func() {
-		s := bufio.NewScanner(stderr)
-		for s.Scan() {
-			fmt.Fprintln(os.Stderr, "stdioproc err:", s.Text())
-		}
-		if err := s.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "stdioproc failed:", err)
-		}
-	}()
 
 	go cmd.Wait()
 
