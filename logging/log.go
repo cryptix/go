291869d0cedd08e766eddff29899ab542a0dfd7d
Henry <cryptix@riseup.net>
2017-01-27 09:21:18 +0000

    log polish

--- logging/log.go
+++ logging/log.go
@@ -3,9 +3,10 @@ package logging
 import (
 	"fmt"
 	"io"
+	stdlog "log"
 	"os"
 
-	"github.com/go-kit/kit/log"
+	kitlog "github.com/go-kit/kit/log"
 	"gopkg.in/errgo.v1"
 )
 
@@ -18,10 +19,10 @@ func SetCloseChan(c chan<- os.Signal) {
 // CheckFatal exits the process if err != nil
 func CheckFatal(err error) {
 	if err != nil {
-		l := Underlying
+		l := internal
 		if l == nil {
-			l = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
-			l = log.NewContext(l).With("module", "logging", log.DefaultCaller)
+			l = kitlog.NewLogfmtLogger(kitlog.NewSyncWriter(os.Stderr))
+			l = kitlog.NewContext(l).With("module", "logging", kitlog.DefaultCaller)
 		}
 
 		l.Log("check", "fatal", "err", errgo.Details(err))
@@ -33,33 +34,40 @@ func CheckFatal(err error) {
 	}
 }
 
-var Underlying log.Logger
+var internal kitlog.Logger
 
 // SetupLogging will initialize the logger backend and set the flags.
 func SetupLogging(w io.Writer) {
-	if w != nil {
-		w = io.MultiWriter(os.Stderr, w)
+	if w == nil {
+		w = os.Stderr
 	}
-	logger := log.NewLogfmtLogger(w)
+	logger := kitlog.NewLogfmtLogger(w)
+
 	if lvl := os.Getenv("CRYPTIX_LOGLVL"); lvl != "" {
 		logger.Log("module", "logging", "error", "CRYPTIX_LOGLVL is obsolete. levels are bad, mkay?")
 	}
 	// wrap logger to error-check the writes only once
-	Underlying = log.LoggerFunc(func(keyvals ...interface{}) error {
+	internal = kitlog.LoggerFunc(func(keyvals ...interface{}) error {
 		if err := logger.Log(keyvals...); err != nil {
 			fmt.Fprintf(os.Stderr, "warning: logger.Write() failed! %s", err)
 			panic(err) // no other way to escalate this
 		}
 		return nil
 	})
+	stdlog.SetOutput(kitlog.NewStdlibAdapter(kitlog.NewContext(internal).With("module", "stdlib")))
+	internal = kitlog.NewContext(internal).With("ts", kitlog.DefaultTimestamp, "caller", kitlog.DefaultCaller)
 }
 
 // Logger returns an Entry where the module field is set to name
-func Logger(name string) *log.Context {
+func Logger(name string) *kitlog.Context {
+	l := internal
+	if l == nil {
+		l = kitlog.NewLogfmtLogger(kitlog.NewSyncWriter(os.Stderr))
+		l = kitlog.NewContext(l).With("warning", "uninitizialized", kitlog.DefaultCaller)
+	}
 	if name == "" {
-		Underlying.Log("module", "logger", "error", "missing name parameter")
+		l.Log("module", "logger", "error", "missing name parameter")
 		name = "undefined"
 	}
-
-	return log.NewContext(Underlying).With("ts", log.DefaultTimestamp, "caller", log.DefaultCaller, "module", name)
+	return kitlog.NewContext(l).With("module", name)
 }
